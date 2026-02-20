4e18375284cd9ad8f8143224f50588e6a718776c
aryank3012 <aryanrajak3012@gmail.com>
2026-02-20 19:55:52 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,18 @@
+let str = "";
+let buttons = document.querySelectorAll(".btn");
+
+Array.from(buttons).forEach((buttons) => {
+    buttons.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=") {
+            str = eval(str);
+            document.querySelector(".screen").value = str;
+        } else if (e.target.innerHTML == "AC") {
+            str = "";
+            document.querySelector(".screen").value = str;
+        } else {
+            console.log(e.target);
+            str = str + e.target.innerHTML;
+            document.querySelector(".screen").value = str;
+        }
+    })
+})
